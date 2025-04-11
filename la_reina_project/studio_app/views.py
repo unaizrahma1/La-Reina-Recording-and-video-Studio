421ee4beb07c19fded2374908b5a7eb699e222d8
unaizrahma1 <unaizrahman10@gmail.com>
2025-04-11 14:22:59 +0530

    view updated

--- la_reina_project/studio_app/views.py
+++ la_reina_project/studio_app/views.py
@@ -7,7 +7,26 @@ def index(request):
     artists = Artist.objects.all()
     return render(request, 'index.html', {'artists': artists})
 
+def add_artist(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        genre = request.POST.get('genre')
+        bio = request.POST.get('bio')
+        contact = request.POST.get('contact')
+        email = request.POST.get('email')
+        image = request.FILES.get('image')
 
+        Artist.objects.create(
+            name=name,
+            genre=genre,
+            bio=bio,
+            contact=contact,
+            email=email,
+            image=image
+        )
+        return redirect('index')
+    
+    return render(request, 'add_artist.html')
 
 def artist_list(request):
     artists = Artist.objects.all()
