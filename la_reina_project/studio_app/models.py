fd5d1a9b7ecb886fb8f99726a6b26e353f397342
unaizrahma1 <unaizrahman10@gmail.com>
2025-04-11 13:21:55 +0530

    booking model added

--- la_reina_project/studio_app/models.py
+++ la_reina_project/studio_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 class Artist(models.Model):
     name = models.CharField(max_length=100)
@@ -10,3 +11,21 @@ class Artist(models.Model):
 
     def __str__(self):
         return self.name
+
+class Booking(models.Model):
+    SESSION_CHOICES = [
+        ('Recording', 'Recording'),
+        ('Mixing', 'Mixing'),
+        ('Mastering', 'Mastering'),
+        ('Practice', 'Practice'),
+        ('Other', 'Other'),
+    ]
+
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name='bookings')
+    date = models.DateField(default=timezone.now)
+    time = models.TimeField()
+    session_type = models.CharField(max_length=50, choices=SESSION_CHOICES)
+    duration = models.DurationField(help_text="Format: hh:mm:ss")
+
+    def __str__(self):
+        return f"{self.artist.name} - {self.session_type} on {self.date} at {self.time}"
